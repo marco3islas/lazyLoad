bd2a6c018baa1f9ce9b05da519f631e09245a1eb
Marco Antonio Islas <marco3islas@gmail.com>
2021-07-30 11:33:55 -0500

    Se modifico el home agregandose el side navbar de material

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 
+import {MatListModule}   from '@angular/material/list';
+import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
 
 
@@ -10,7 +12,9 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     exports: [
         MatButtonModule,
         MatIconModule,
-        MatToolbarModule
+        MatToolbarModule,
+        MatListModule,
+        MatSidenavModule
     ]
 })
 export class MaterialModule { }
